e5dd825eee636a3afc3eb8e2448d077313bd2e7e
Y3abtsega <yeabtsega.aragaw@bitscollege.edu.et>
2025-02-11 01:27:19 +0300

    The following changes made create the search, notifications , and cast icons on the AppBar.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,6 +14,20 @@ class HomeScreen extends StatelessWidget {
             padding: const EdgeInsets.only(left: 12.0),
             child: Image.asset('assets/yt_logo_dark.png'),
           ),
+          actions: [
+              IconButton(
+                icon: Icon(Icons.cast),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.notification_add_outlined),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(Icons.search),
+                onPressed: () {},
+              )
+            ],
         ),
       ],
     ));

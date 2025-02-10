e55d5802175d068c8e3abc6d293a556f48e476f2
Y3abtsega <yeabtsega.aragaw@bitscollege.edu.et>
2025-02-11 01:19:13 +0300

    This code creates a HomeScreen with a SliverAppBar inside a scrollable CustomScrollView, displaying an image (yt_logo_dark.png) as the leading widget with left padding. The SliverAppBar allows for a flexible, scrollable app bar that can expand, collapse, or stay fixed based on user interaction.

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,6 +5,17 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        body: CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          leadingWidth: 100.0,
+          leading: Padding(
+            padding: const EdgeInsets.only(left: 12.0),
+            child: Image.asset('assets/yt_logo_dark.png'),
+          ),
+        ),
+      ],
+    ));
   }
 }
